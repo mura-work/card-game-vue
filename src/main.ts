bfca98f1706e0a6bb3d2b58502277e8512d78601
mura-work <murata.naoya.work@gmail.com>
2023-04-03 23:16:37 +0900

    vuetifyの導入

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import vuetify from "./plugins/vuetify"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(vuetify).mount('#app')
