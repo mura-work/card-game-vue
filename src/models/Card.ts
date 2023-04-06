2d956db71cadaa4c72eb9189e85a0793872c1f80
mura-work <49024168+mura-work@users.noreply.github.com>
2023-04-06 22:43:35 +0900

    Feature brack jack (#2)
    * [feat] 画像の準備とCardクラスの作成
    
    * [feat] デッキモデルの作成と盤面の作成 WIP

---
+++ src/models/Card.ts
@@ -0,0 +1,32 @@
+export default class Card {
+  private readonly _suit: string;
+  private readonly _rank: string;
+  private readonly _imageId: number;
+
+  constructor(suit: string, rank: string, imageId: number) {
+    this._suit = suit;
+    this._rank = rank;
+		this._imageId = imageId;
+  }
+
+  get suit() {
+    return this._suit;
+  }
+
+  get rank() {
+    return this._rank;
+  }
+
+	/**
+	 * カードの画像を表示する際に使用
+	 * @readonly
+	 * @memberof Card
+	 */
+	get imageId() {
+		return this._imageId;
+	}
+
+  getRankNumber(): number {
+    return Number(this._rank);
+  }
+}
