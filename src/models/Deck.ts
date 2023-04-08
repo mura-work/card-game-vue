976c2ad2987112ff2efaecd2ea5829afae15f7fe
mura-work <49024168+mura-work@users.noreply.github.com>
2023-04-08 22:37:15 +0900

    Feature brack jack (#3)
    * [feat] 画像の準備とCardクラスの作成
    
    * [feat] デッキモデルの作成と盤面の作成 WIP
    
    * [feat] デッキをシャッフルし、ディーラー・プレイヤーともに2枚表示されている状態を作成
    
    * [feat] 掛け金を入力フェーズの作成

--- src/models/Deck.ts
+++ src/models/Deck.ts
@@ -15,9 +15,26 @@ export default class Deck {
         return [...Array(13)].map((_, i) => {
           const imageId = idx * 13 + (i + 1);
           return new Card(suit, String(i), imageId);
-        })
-			}
-      )
+        });
+      })
       .flat();
+		this.shuffle()
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; 0 < i; i--) {
+      let r = Math.floor(Math.random() * (i + 1));
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[r];
+      this.cards[r] = temp;
+    }
+  }
+
+  /**
+   * デッキから1枚引く
+   * @return { Card }
+   */
+  pick(): Card {
+    return this.cards.splice(0, 1)[0];
   }
 }
--- src/pages/BrackJack.vue
+++ src/pages/BrackJack.vue
@@ -1,53 +1,74 @@
 <script setup lang="ts">
 import Deck from "../models/Deck.ts";
-import { onMounted } from "vue";
-import SampleCard1 from "../assets/images/cards/torannpu-illust1.png";
-import SampleCard2 from "../assets/images/cards/torannpu-illust2.png";
+import { onMounted, reactive, ref } from "vue";
+import { imageUrl } from "../utils/index.ts";
+const CONST_SCENE = {
+  BETTING: "betting", // ベット
+  ACTIONS: "actions", // 各アクションの実施
+  RESULT: "result", // 最終結果
+};
+
+type SceneType = "betting" | "actions" | "result";
+let scene = ref<SceneType>(CONST_SCENE.BETTING);
+const bettingPrice = ref(0);
+const dealerHands = reactive([]);
+const plyaerHands = reactive([]);
 
 onMounted(() => {
   const deck = new Deck();
   console.log({ deck });
+  dealerHands.push(...[deck.pick(), deck.pick()]);
+  plyaerHands.push(...[deck.pick(), deck.pick()]);
 });
+
+const confirmBettingPrice = () => {
+	console.log({bettingPrice}, bettingPrice.value)
+	if (bettingPrice.value <= 0) return
+	scene.value = CONST_SCENE.ACTIONS
+}
 </script>
 
 <template>
   <div class="h-screen w-screen bg-green-800 flex justify-center flex-col">
-    <div class="w-3/5 max-w-3/5 w-auto flex justify-center flex-col">
-      <h2 class="text-white font-bold text-4xl">Dealer</h2>
-      <div class="w-1/2">
-        <v-img
-          class="font-bold inline-block mr-8"
-          height="180"
-          width="120"
-          :src="SampleCard1"
-          cover
-        />
-        <v-img
-          class="font-bold inline-block"
-          height="180"
-          width="120"
-          :src="SampleCard1"
-          cover
-        />
+    <div class="w-3/5 max-w-3/5 w-auto text-center mb-16">
+      <h2 class="text-white font-bold text-4xl mb-8">Dealer</h2>
+      <div v-if="scene === CONST_SCENE.ACTIONS" class="w-auto h-auto">
+        <template v-for="card in dealerHands" :key="card.imageId">
+          <v-img
+            class="font-bold inline-block mr-8"
+            height="180"
+            width="120"
+            :src="imageUrl(card.imageId)"
+            cover
+          />
+        </template>
       </div>
     </div>
-    <div class="max-w-3/5 w-auto w-3/5">
-      <h2 class="text-white font-bold text-4xl">Player</h2>
-			<div class="w-auto">
-        <v-img
-          class="font-bold inline-block mr-8"
-          height="180"
-          width="120"
-          :src="SampleCard1"
-          cover
-        />
-        <v-img
-          class="font-bold inline-block"
-          height="180"
-          width="120"
-          :src="SampleCard1"
-          cover
-        />
+    <div class="max-w-3/5 w-auto w-3/5 text-center">
+      <h2 class="text-white font-bold text-4xl mb-8">Player</h2>
+      <div v-if="scene === CONST_SCENE.BETTING">
+				<div class="flex justify-center mb-8">
+					<input
+						v-model="bettingPrice"
+						class="bg-white text-right h-7"
+						type="number"
+						min="1"
+						max="10000"
+					/>
+					<v-btn @click="confirmBettingPrice" :disabled="bettingPrice <= 0" class="ml-4 bg-blue" size="small">確定</v-btn>
+				</div>
+				<span class="text-white font-bold">掛け金を入力してください。<br>1ptから10000ptまでかけることができます.</span>
+      </div>
+      <div v-if="scene === CONST_SCENE.ACTIONS" class="w-auto h-auto">
+        <template v-for="card in plyaerHands" :key="card.imageId">
+          <v-img
+            class="font-bold inline-block mr-8"
+            height="180"
+            width="120"
+            :src="imageUrl(card.imageId)"
+            cover
+          />
+        </template>
       </div>
     </div>
   </div>
