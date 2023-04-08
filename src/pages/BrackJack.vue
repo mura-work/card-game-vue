964e0b981bced0c235d5a4112b92939df6d7dd97
mura-work <murata.naoya.work@gmail.com>
2023-04-08 23:09:15 +0900

    [feat] スタンドの動線とrankの値の修正

--- src/pages/BrackJack.vue
+++ src/pages/BrackJack.vue
@@ -8,24 +8,61 @@ const CONST_SCENE = {
   RESULT: "result", // 最終結果
 };
 
+const CONST_ACTION = {
+  SURRENDER: "surrender",
+  STAND: "stand",
+  HIT: "hit",
+  DOUBLE: "double",
+};
+
 type SceneType = "betting" | "actions" | "result";
+type ActionType = "surrender" | "stand" | "hit" | "double";
 let scene = ref<SceneType>(CONST_SCENE.BETTING);
-const bettingPrice = ref(0);
+let playerPoint = ref<number>(1000); // 初期値 ローカルセッションで保持する
+const bettingPoint = ref(0);
 const dealerHands = reactive([]);
-const plyaerHands = reactive([]);
+const playerHands = reactive([]);
 
 onMounted(() => {
   const deck = new Deck();
   console.log({ deck });
   dealerHands.push(...[deck.pick(), deck.pick()]);
-  plyaerHands.push(...[deck.pick(), deck.pick()]);
+  playerHands.push(...[deck.pick(), deck.pick()]);
 });
 
-const confirmBettingPrice = () => {
-	console.log({bettingPrice}, bettingPrice.value)
-	if (bettingPrice.value <= 0) return
-	scene.value = CONST_SCENE.ACTIONS
-}
+const confirmBettingPoint = () => {
+  console.log({ bettingPoint }, bettingPoint.value);
+  if (bettingPoint.value <= 0) {
+    console.log("ポイントが入力されていません");
+    return;
+  } else if (bettingPoint.value > playerPoint.value) {
+    console.log(
+      "所持ポイントよりも入力ポイントの方が多いです。所持ポイントを上回らないようにしてください"
+    );
+    return;
+  }
+  scene.value = CONST_SCENE.ACTIONS;
+};
+
+const actions = (actions: ActionType) => {
+  console.log({ actions });
+};
+
+const calculateHandTotal = (hands: Hand[]) => {
+  return hands.reduce((prev, cur) => {
+    const rank = Number(cur.rank);
+    const score = rank > 10 ? 10 : rank;
+    return prev + score;
+  }, 0);
+};
+
+const judge = async () => {
+  const [dealerScore, playerScore] = await Promise.all(
+    [dealerHands, playerHands].map((hands) => calculateHandTotal(hands))
+  );
+  const result = playerScore > dealerScore;
+  console.log({ result, playerScore, dealerScore });
+};
 </script>
 
 <template>
@@ -45,22 +82,33 @@ const confirmBettingPrice = () => {
       </div>
     </div>
     <div class="max-w-3/5 w-auto w-3/5 text-center">
-      <h2 class="text-white font-bold text-4xl mb-8">Player</h2>
+      <h2 class="text-white font-bold text-4xl mb-4">Player</h2>
       <div v-if="scene === CONST_SCENE.BETTING">
-				<div class="flex justify-center mb-8">
-					<input
-						v-model="bettingPrice"
-						class="bg-white text-right h-7"
-						type="number"
-						min="1"
-						max="10000"
-					/>
-					<v-btn @click="confirmBettingPrice" :disabled="bettingPrice <= 0" class="ml-4 bg-blue" size="small">確定</v-btn>
-				</div>
-				<span class="text-white font-bold">掛け金を入力してください。<br>1ptから10000ptまでかけることができます.</span>
+        <div class="text-white font-bold mb-4">
+          現在の所持Pt: {{ playerPoint }}
+        </div>
+        <div class="flex justify-center mb-8">
+          <input
+            v-model="bettingPoint"
+            class="bg-white text-right h-7"
+            type="number"
+            min="1"
+            :max="playerPoint"
+          />
+          <v-btn
+            @click="confirmBettingPoint"
+            :disabled="bettingPoint <= 0"
+            class="ml-4 bg-blue"
+            size="small"
+            >確定</v-btn
+          >
+        </div>
+        <span class="text-white font-bold"
+          >掛け金を入力してください。<br />1ptから10000ptまでかけることができます.</span
+        >
       </div>
-      <div v-if="scene === CONST_SCENE.ACTIONS" class="w-auto h-auto">
-        <template v-for="card in plyaerHands" :key="card.imageId">
+      <div v-if="scene === CONST_SCENE.ACTIONS" class="w-auto h-auto mb-8">
+        <template v-for="card in playerHands" :key="card.imageId">
           <v-img
             class="font-bold inline-block mr-8"
             height="180"
@@ -69,6 +117,18 @@ const confirmBettingPrice = () => {
             cover
           />
         </template>
+      </div>
+      <div v-if="scene === CONST_SCENE.ACTIONS">
+        <v-btn @click="actions(CONST_ACTION.SURRENDER)" class="mr-4 bg-red"
+          >サレンダー</v-btn
+        >
+        <v-btn @click="judge()" class="mr-4 bg-yellow">スタンド</v-btn>
+        <v-btn @click="actions(CONST_ACTION.HIT)" class="mr-4 bg-green"
+          >ヒット</v-btn
+        >
+        <v-btn @click="actions(CONST_ACTION.DOUBLE)" class="mr-4 bg-blue"
+          >ダブル</v-btn
+        >
       </div>
     </div>
   </div>
